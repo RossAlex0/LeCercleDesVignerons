2b38be3c575fbf80c39c43979c952d2a590edcaa
Rossignol Alex <alexnw33910@gmail.com>
2025-06-18 20:27:23 +0200

    feat: add Hero and WinesSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import Header from "@/components/blocks/header";
-import Landing from "@/components/layouts/landing";
+import Hero from "@/components/layouts/hero";
+import WinesSection from "@/components/layouts/wines-section";
 
 export default function Home() {
   return (
     <>
       <Header />
       <main>
-        <Landing />
+        <Hero />
+        <WinesSection />
       </main>
     </>
   );
