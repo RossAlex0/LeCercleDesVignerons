e2551f90a4c61362783cb94f28d54738f5e204b5
Rossignol Alex <alexnw33910@gmail.com>
2025-06-18 20:31:08 +0200

    fix: up styles into arrow-down component and add disabled props into button

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -4,15 +4,21 @@ import { MotionButtonProps } from "./type";
 import "./button.css";
 
 export default function Button({ children, ...props }: MotionButtonProps) {
+  const isDisabled = props.disabled ?? false;
+
   return (
     <motion.button
       className="motion_button"
       type="button"
-      whileHover={{
-        scale: 1.05,
-        boxShadow: "0 0 15px rgba(191,165,90,0.4)",
-      }}
-      transition={{ type: "spring", stiffness: 300 }}
+      whileHover={
+        !isDisabled
+          ? {
+              scale: 1.05,
+              boxShadow: "0 0 15px rgba(191,165,90,0.4)",
+            }
+          : undefined
+      }
+      transition={!isDisabled ? { type: "spring", stiffness: 300 } : undefined}
       {...props}
     >
       {children}
