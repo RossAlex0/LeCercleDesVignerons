458c21093e4f102340b86330f850396ad5a896e5
Rossignol Alex <alexnw33910@gmail.com>
2025-06-19 23:04:20 +0200

    fix: disabled options

--- src/components/layouts/contact-modal/index.tsx
+++ src/components/layouts/contact-modal/index.tsx
@@ -2,6 +2,8 @@ import { motion, AnimatePresence } from "framer-motion";
 
 import "./contact-modal.css";
 import Input from "@/components/ui/input";
+import Button from "@/components/ui/button";
+import { useState } from "react";
 
 export default function ModalContact({
   isOpen,
@@ -10,6 +12,15 @@ export default function ModalContact({
   isOpen: boolean;
   setIsOpen: (state: boolean) => void;
 }) {
+  const [form, setForm] = useState({
+    name: "",
+    subject: "",
+    email: "",
+    message: "",
+  });
+
+  const isDisabled =
+    !!form.name && !!form.subject && !!form.message && !!form.email;
   return (
     <AnimatePresence>
       {isOpen && (
@@ -28,21 +39,59 @@ export default function ModalContact({
             exit={{ scale: 0, opacity: 0 }}
             transition={{ duration: 0.7 }}
           >
-            <h2>Ma super modal 🍷</h2>
-            <Input />
-            <button
-              onClick={() => setIsOpen(false)}
-              style={{
-                marginTop: "1rem",
-                backgroundColor: "#333",
-                color: "white",
-                padding: "10px 15px",
-                borderRadius: "5px",
-                border: "none",
-              }}
-            >
-              Fermer
-            </button>
+            <h2 className="input_title">Ecrivez-nous !</h2>
+            <div className="input_container">
+              <div className="half_input">
+                <Input
+                  stateTools={{
+                    state: form.name,
+                    setState: (value) => setForm({ ...form, name: value }),
+                  }}
+                  half
+                  placeholder="Nom / Prénom"
+                />
+                <Input
+                  stateTools={{
+                    state: form.email,
+                    setState: (value) => setForm({ ...form, email: value }),
+                  }}
+                  half
+                  placeholder="Mail"
+                />
+              </div>
+              <Input
+                stateTools={{
+                  state: form.subject,
+                  setState: (value) => setForm({ ...form, subject: value }),
+                }}
+                placeholder="Objet"
+              />
+              <Input
+                stateTools={{
+                  state: form.message,
+                  setState: (value) => setForm({ ...form, message: value }),
+                }}
+                area
+                placeholder="Message"
+              />
+            </div>
+            <div className="modal_button">
+              <Button
+                disabled={!isDisabled}
+                style={!isDisabled ? { opacity: "0.4" } : { opacity: "1" }}
+              >
+                Envoyer
+              </Button>
+              <Button
+                style={{
+                  backgroundColor: "#121111",
+                  border: "1px solid #b6935e",
+                }}
+                onClick={() => setIsOpen(false)}
+              >
+                Fermer
+              </Button>
+            </div>
           </motion.div>
         </motion.div>
       )}
